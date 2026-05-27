c3438f734d28cbc7a8241048bf1e6bf6168eb532
Ravindra Kumar <ravindraietbu@gmail.com>
2026-05-27 15:30:11 +0530

    fix the  the  route of pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/dexkor-optin-page',
+        source: '/dexkor-new-lp',
         destination: '/external-pages/dexkor-optin-page',
       },
       {
-        source: '/dexkor-new-lp',
+        source: '/dexkor-optin-page',
         destination: '/external-pages/dexkor-new-lp',
-      },
-      {
-        source: '/dexkor-new-lp/thank-you',
-        destination: '/external-pages/dexkor-new-lp/thank-you',
       },
       {
         source: '/dexkor-thank-you',
