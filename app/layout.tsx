5e0cb593ccb282131207d45e01da350525732f60
Ravindra Kumar <ravindraietbu@gmail.com>
2026-06-17 19:47:22 +0530

    applo script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -166,6 +166,14 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        <Script id="apollo-tracking" strategy="afterInteractive">
+          {`
+            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+            o.onload=function(){window.trackingFunctions.onLoad({appId:"6a141dd3a319410020332cf1"})},
+            document.head.appendChild(o)}initApollo();
+          `}
+        </Script>
         <ThemeProvider>
           <Navbar />
           <div className="pt-16">
